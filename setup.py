8ab32686e0a522f2284a7136ecbc67e81c42f40c
Premanath Talamarla <premanath@syoft.com>
2023-11-24 18:48:37 +0530

    0.1.7

--- setup.py
+++ setup.py
@@ -13,11 +13,19 @@
 
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name='PremaDjango',
-    version='0.1.6',
+    version='0.1.7',
     packages=find_packages(),
     install_requires=[
         'Django',  # Add other default libraries here
     ],
+    author="Premanath",
+    author_email="talamarlapremanath143@gmail.com",
+    description="My Short Description",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
 )
