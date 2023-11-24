8f0c0df69a2876ff4f5f63ed40b0d6807113d638
Premanath Talamarla <premanath@syoft.com>
2023-11-24 16:50:05 +0530

    Use setuptools_scm for versioning

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='your-package-name',
+    name='PremaDjango',
     use_scm_version=True,
     setup_requires=['setuptools_scm'],
     packages=find_packages(),
