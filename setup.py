94f8440e076690198e4975a42927c3a183ab15fb
Premanath Talamarla <premanath@syoft.com>
2023-11-24 19:02:17 +0530

    0.1.9

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='PremaDjango',
-    version='0.1.8',
+    version='0.1.9',
     packages=find_packages(),
     install_requires=[
         'Django',  # Add other default libraries here
