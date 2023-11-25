c0c5745c8bc36a877257053b44a1536034ae64c5
Premanath Talamarla <premanath@syoft.com>
2023-11-25 19:20:02 +0530

    dynamic version 3

--- update_version.py
+++ update_version.py
@@ -3,22 +3,27 @@
 import subprocess
 from setuptools_scm import get_version
 
-
 def get_changed_files():
     # Use git to get the list of changed files
     changed_files_command = "git diff --name-only --diff-filter=AM HEAD^ HEAD"
     result = subprocess.run(changed_files_command, shell=True, check=True, capture_output=True, text=True)
     return result.stdout.strip().split("\n")
 
-
 def count_changed_pages(changed_files):
     # Count all changed files, including added ones
     return len(changed_files)
 
-
 def update_version():
+    # Get the version from setuptools_scm
     current_version = get_version()
-    major, minor, patch = map(int, current_version.split('.')[:3])
+
+    # Extract only major, minor, and patch
+    version_parts = current_version.split('.')
+    if len(version_parts) >= 3:
+        major, minor, patch = map(int, version_parts[:3])
+    else:
+        # Handle the case where the version string doesn't have major, minor, and patch
+        raise ValueError("Invalid version format")
 
     changed_files = get_changed_files()
     changed_page_count = count_changed_pages(changed_files)
@@ -26,15 +31,17 @@ def update_version():
     if changed_page_count <= 2:
         patch += 1
     else:
-        major = 0
-        minor += 1
+        major += 1
+        minor = 0
         patch = 0
 
     new_version = f"{major}.{minor}.{patch}"
 
     return new_version
 
-
 if __name__ == "__main__":
-    new_version = update_version()
-    print("Updated Version:", new_version)
+    try:
+        new_version = update_version()
+        print("Updated Version:", new_version)
+    except ValueError as e:
+        print(f"Error: {e}")
